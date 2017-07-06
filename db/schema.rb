842f8297894f6c2dd1382e268ac365dd625ea7c5
dzhiqin <dzhiqin@126.com>
2017-07-06 10:08:31 +0800

    show jos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705102156) do
+ActiveRecord::Schema.define(version: 20170706011150) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.text     "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
