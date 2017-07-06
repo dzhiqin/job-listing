bb3a02b6b19f3228ecbc8d887daa1677d514953b
dzhiqin <dzhiqin@126.com>
2017-07-06 16:02:51 +0800

    add public and hidden label in job title

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,7 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only:[:new,:update,:edit,:destroy]
   def index
-    @jobs=Job.all
+    @jobs=Job.where(:is_hidden => false).order("created_at DESC")
   end
 
   def new
@@ -42,7 +42,8 @@ class JobsController < ApplicationController
 
   private
   def job_params
-    params.require(:job).permit(:title,:description,:wage_lower_bound,:wage_upper_bound,:contact_email)
+    params.require(:job).permit(:title,:description,:wage_lower_bound,:wage_upper_bound,
+    :contact_email,:is_hidden)
   end
 
 end
