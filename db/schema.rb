8aad57acd097e94c69fc1ae4cb70c665e3fa1ec4
dzhiqin <dzhiqin@126.com>
2017-07-07 17:45:16 +0800

    upload test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707080836) do
+ActiveRecord::Schema.define(version: 20170707092125) do
 
   create_table "jobs", force: :cascade do |t|
     t.text     "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170707080836) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "attachment"
   end
 
   create_table "users", force: :cascade do |t|
